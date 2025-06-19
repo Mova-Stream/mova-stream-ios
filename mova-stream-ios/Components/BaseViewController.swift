eb815d73161b78377295e80acfc015b83ffc2c01
soyong <soyoong.dev@gmail.com>
2025-06-19 17:56:12 +0700

    Update draw storyboard HomeVC

--- mova-stream-ios/Components/BaseViewController.swift
+++ mova-stream-ios/Components/BaseViewController.swift
@@ -12,69 +12,105 @@ public class BaseViewController: UIViewController {
     
     private var leftAction: (() -> Void)?
     private var rightAction: (() -> Void)?
-    
-    // MARK: - Lifecycle
-    
-    /// 0
-    /// Called first when initializing a view controller
-    /// Eg: initialize the UI
-    public override func loadView() {
-        super.loadView()
-    }
-    
-    /// 1
-    /// When the view controller has been loaded into memory (provided that this view controller does not already exist in memory), the viewDidLoad function is called.
-    /// Called only once in the life of that view.
-    /// Often used to prepare data or initialize default values for objects and UI on the screen.
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        self.configureView()
-    }
-    
-    /// 2
-    /// Called before a view is added to the view system and before the animation renders a view.
-    /// At the time of animation to display the view, if you want to customize something, this function will help you do it.
-    public override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // Make sure the top constraint of the TableView is equal to Superview and not Safe Area
-        // Hide the navigation bar completely
-        // Make the Navigation Bar background transparent
-    }
-    
-    /// 3
-    /// The function will be called when a view has been added to several view systems and displayed on the screen.
-    public override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-    
-    /// 4
-    /// Called when a view has been hidden from the screen and amination when the view is hidden.
-    public override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-    
-    /// 5
-    /// Called when a view has been hidden from the screen.
-    public override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
-    
-    public override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-    }
+//    
+//    // MARK: - Lifecycle
+//    
+//    /// 0
+//    /// Called first when initializing a view controller
+//    /// Eg: initialize the UI
+//    public override func loadView() {
+//        super.loadView()
+//    }
+//    
+//    /// 1
+//    /// When the view controller has been loaded into memory (provided that this view controller does not already exist in memory), the viewDidLoad function is called.
+//    /// Called only once in the life of that view.
+//    /// Often used to prepare data or initialize default values for objects and UI on the screen.
+//    public override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//    
+//    /// 2
+//    /// Called before a view is added to the view system and before the animation renders a view.
+//    /// At the time of animation to display the view, if you want to customize something, this function will help you do it.
+//    public override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        // Make sure the top constraint of the TableView is equal to Superview and not Safe Area
+//        // Hide the navigation bar completely
+//        // Make the Navigation Bar background transparent
+//    }
+//    
+//    /// 3
+//    /// The function will be called when a view has been added to several view systems and displayed on the screen.
+//    public override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }
+//    
+//    /// 4
+//    /// Called when a view has been hidden from the screen and amination when the view is hidden.
+//    public override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//    }
+//    
+//    /// 5
+//    /// Called when a view has been hidden from the screen.
+//    public override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//    }
+//    
+//    public override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        
+//    }
 }
 
 
 extension BaseViewController {
     
-    // MARK: - Configuration
-    
-    private func configureView() {
-        view.backgroundColor = .baseBackgroundColor
+    // MARK: - Navigation Bar Control
+    // Sets the navigation bar to be transparent and hides the shadow
+    /**
+        Using example:
+        ```swift
+         override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.setNavigationBarTransparent()
+         }
+        ```
+     */
+    func setNavigationBarTransparent() {
+        // Make the Navigation Bar background transparent
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.tintColor = .white
     }
     
-    // MARK: - Navigation Bar Control
+    // Remove 'Back' text and Title from Navigation Bar
+    /**
+        Using example:
+        ```swift
+         override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.removeBackTextAndTitle()
+         }
+        ```
+     */
+    func setNavigationBackTextAndTitleHidden() {
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.navigationItem.title = ""
+    }
+    
+    // Hide the navigation bar completely
+    /**
+        Using example:
+        ```swift
+         override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.hideNavigationBar()
+         }
+        ```
+     */
     
     public func setNavigationTitle(_ title: String, color: UIColor = .label) {
         let titleLabel = UILabel()
--- mova-stream-ios/ViewControllers/Cors/HomeVC/HomeViewController.swift
+++ mova-stream-ios/ViewControllers/Cors/HomeVC/HomeViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SwiftUI
 import ComponentsKit
 import AutoLayout
 
@@ -16,20 +15,14 @@ class HomeViewController: BaseViewController {
     
     // MARK: - Properties
     
-    private var scrollViewStretchyHeaderView: ScrollViewWithStretchyHeaderView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
-        self.setupScrollViewStretchyHeader()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.setNavigationBarBackgroundColor(.clear)
-        
-        let blur = UIBlurEffect(style: .dark)
-        self.setNavigationBarBackgroundBlurWhenScroll(blur)
+        self.hideNavigationBar()
     }
 }
 
@@ -96,53 +89,4 @@ extension HomeViewController {
         self.setRightBarItem(view: stackView)
     }
     
-    private func setupScrollViewStretchyHeader() {
-        scrollViewStretchyHeaderView = ScrollViewWithStretchyHeaderView()
-        scrollViewStretchyHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.view.addSubview(scrollViewStretchyHeaderView)
-        NSLayoutConstraint.activate([
-            scrollViewStretchyHeaderView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollViewStretchyHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollViewStretchyHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollViewStretchyHeaderView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        
-        scrollViewStretchyHeaderView.imageView.image = UIImage(named: "doctor_strange2")
-        
-        let titleLabel = UILabel()
-        let descriptionLabel = UILabel()
-        
-        titleLabel.text = "Movie Title"
-        titleLabel.font = .boldSystemFont(ofSize: 24)
-        titleLabel.textColor = .label
-        
-        descriptionLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor risus sapien, eu mattis eros cursus a. Ut auctor ipsum ut placerat vehicula. Nulla sed risus nec neque placerat imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend ante vel purus condimentum, sit amet congue nibh rutrum. Donec tristique sollicitudin nulla, quis consectetur purus bibendum ut. Nam finibus eleifend erat in eleifend. Nam nunc nibh, molestie vel dui sit amet, viverra consequat felis. Curabitur hendrerit rutrum convallis. Nulla facilisi. Mauris pretium et dolor vitae vestibulum. \n\nNullam facilisis nisi a sagittis consectetur. Proin tristique rutrum lobortis. Nam vitae mauris in ex convallis placerat. Pellentesque sed sodales diam. Fusce volutpat orci tristique justo fringilla, a efficitur urna fermentum. Maecenas sagittis tellus sed lorem maximus, at blandit enim vestibulum. Ut ac tempus diam, ut lobortis ipsum. Mauris sodales nibh laoreet arcu vestibulum, quis tincidunt ipsum mattis. Nunc eleifend justo a purus porta varius. Nulla facilisi. Cras et cursus magna. \n\nDonec id nibh nunc. Mauris efficitur sem et turpis pellentesque, faucibus venenatis mauris pulvinar. Sed sed porta ligula, eu commodo elit. Nam congue tempor odio, a porta nisi dignissim sit amet. Vestibulum sed egestas mauris. Aenean vitae nisi quis mauris hendrerit blandit. Fusce ante sem, auctor et posuere at, porttitor vel dolor. \n\nNulla elementum posuere rhoncus. In vestibulum sit amet neque at malesuada. Duis suscipit vehicula enim at pretium. Phasellus non venenatis tellus. Vivamus ut arcu dignissim, vehicula lorem et, pharetra risus. Donec ut interdum ante. Fusce feugiat eget ante nec commodo. Nulla at purus rutrum, commodo metus quis, aliquet eros. Suspendisse consequat sem at nibh vulputate, non elementum purus molestie. Vivamus sit amet euismod dolor, ac ornare mi. Pellentesque semper tincidunt dolor vitae laoreet. Duis dapibus odio nec nunc facilisis ullamcorper. Nulla facilisi. Duis nec velit sollicitudin, mollis lectus et, lobortis est. \n\nMorbi ac aliquam risus. Nunc ac nisi aliquam, porta arcu vel, porttitor dui. Aliquam lectus dui, pellentesque vitae commodo a, suscipit id velit. Nunc sed libero sit amet magna aliquam congue et quis neque. Praesent suscipit aliquam enim, in vulputate mi commodo efficitur. Nunc semper aliquam viverra. Aliquam erat volutpat. Mauris magna leo, egestas eget ex et, facilisis ornare lectus."
-        descriptionLabel.font = .systemFont(ofSize: 16)
-        descriptionLabel.textColor = .secondaryLabel
-        descriptionLabel.numberOfLines = 0
-        
-        scrollViewStretchyHeaderView.contentView.addSubview(titleLabel)
-        scrollViewStretchyHeaderView.contentView.addSubview(descriptionLabel)
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: scrollViewStretchyHeaderView.contentView.topAnchor, constant: 16),
-            titleLabel.leadingAnchor.constraint(equalTo: scrollViewStretchyHeaderView.contentView.leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: scrollViewStretchyHeaderView.contentView.trailingAnchor),
-            
-            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
-            descriptionLabel.leadingAnchor.constraint(equalTo: scrollViewStretchyHeaderView.contentView.leadingAnchor),
-            descriptionLabel.trailingAnchor.constraint(equalTo: scrollViewStretchyHeaderView.contentView.trailingAnchor),
-            descriptionLabel.bottomAnchor.constraint(lessThanOrEqualTo: scrollViewStretchyHeaderView.contentView.bottomAnchor)
-        ])
-    }
-}
-
-struct HomeViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        return PreviewUIViewController(viewController: HomeViewController())
-    }
 }
